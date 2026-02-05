c75fcf6ef417057a7d08bf41698b72180d313dcb
tayyaba123-com <hilanapoloa236@gmail.com>
2026-02-05 13:40:47 +0500

    Delete day-7 directory

--- day-7/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectToDb() {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("connected to database");
-  });
-}
-
-module.exports = connectToDb;
